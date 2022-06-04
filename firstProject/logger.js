793d14041b3561b69896ebdd39419464543cde31
hassansaber <hassansaberkarin@gmail.com>
2022-06-04 19:44:07 +0430

    module.exports

---
+++ firstProject/logger.js
@@ -0,0 +1,10 @@
+const text = "js";
+
+const sayHi = (name) => {
+  console.log(`this works with ${name}`);
+};
+
+module.exports = {
+  text,
+  sayHi,
+};
---
+++ firstProject/main.js
@@ -0,0 +1,8 @@
+const logger = require("./logger");
+
+console.log("first node project");
+
+console.log(module);
+
+logger.sayHi("jasem");
+// console.log();
